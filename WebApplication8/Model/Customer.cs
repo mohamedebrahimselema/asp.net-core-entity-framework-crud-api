c23fc78fd6fc9a3c0cb6fb6340945d7e28e9f5e6
mohamed selema <mohamed.IBRAHIEM5905@ci.menofia.edu.eg>
2020-12-14 10:02:18 +0200

    Add project files.

---
+++ WebApplication8/Model/Customer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication8.Model
+{
+    public class Customer
+    {
+        [Key]
+        public int Id{ get; set; }
+        
+
+        public string Name{ get; set; }
+
+        public string Country { get; set; }
+
+        [EmailAddress]
+        public string Email{ get; set; }
+
+    }
+}
---
+++ WebApplication8/Model/ShopDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+namespace WebApplication8.Model
+{
+    public class ShopDbContext : DbContext
+    {
+        public ShopDbContext(DbContextOptions<ShopDbContext> options)
+                : base(options)
+        {
+        }
+
+        public DbSet<Customer> Customer { get; set; }
+    }
+}
